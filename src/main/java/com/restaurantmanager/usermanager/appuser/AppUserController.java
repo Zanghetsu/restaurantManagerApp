a406c3dab56714e5862ca66959f04d1145e2be97
Zanghetsu <boythasx@gmail.com>
2021-10-31 17:03:42 +0100

    Created Service layer

--- src/main/java/com/restaurantmanager/usermanager/appuser/AppUserController.java
+++ src/main/java/com/restaurantmanager/usermanager/appuser/AppUserController.java
@@ -1,20 +1,25 @@
 package com.restaurantmanager.usermanager.appuser;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @RestController
 public class AppUserController {
 
+    private final AppUserService appUserService;
+
+    @Autowired
+    public AppUserController(AppUserService appUserService) {
+        this.appUserService = appUserService;
+    }
+
     @GetMapping(path = "api/v1/get_user")
     public List<AppUser> get_AppUsers() {
-        return List.of(
-                new AppUser(1, "bob", "yosef", "boby.yosef@mail.com", LocalDate.of(1990, 12, 1)),
-                new AppUser(2, "bob", "yosef", "boby.yosef@mail.com", LocalDate.of(1990, 12, 1))
-        );
+        return appUserService.get_Users();
+
 
     }
 }
---
+++ src/main/java/com/restaurantmanager/usermanager/appuser/AppUserService.java
@@ -0,0 +1,17 @@
+package com.restaurantmanager.usermanager.appuser;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class AppUserService{
+    public List<AppUser> get_Users() {
+        return List.of(
+                new AppUser(1, "bob", "yosef", "boby.yosef@mail.com", LocalDate.of(1990, 12, 1)),
+                new AppUser(2, "bob", "yosef", "boby.yosef@mail.com", LocalDate.of(1990, 12, 1))
+        );
+    }
+}
