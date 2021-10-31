a2aa00e926bdd6e7d081497e8201066a757a3472
Zanghetsu <boythasx@gmail.com>
2021-10-31 14:32:27 +0100

    First Entity Class setup, with basic restController

--- src/main/java/com/restaurantmanager/usermanager/DemoApplication.java
+++ src/main/java/com/restaurantmanager/usermanager/DemoApplication.java
@@ -1,13 +1,28 @@
 package com.restaurantmanager.usermanager;
 
+import com.restaurantmanager.usermanager.appuser.AppUser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping
+	public List<AppUser> hello(){
+		return List.of(
+				new AppUser( 1, "bob", "yosef", "boby.yosef@mail.com", LocalDate.of(1990,12,1)),
+				new AppUser( 2, "bob", "yosef", "boby.yosef@mail.com", LocalDate.of(1990,12,1))
+		);
 	}
 
 }
---
+++ src/main/java/com/restaurantmanager/usermanager/appuser/AppUser.java
@@ -0,0 +1,28 @@
+package com.restaurantmanager.usermanager.appuser;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class AppUser {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String emailAdress;
+    private LocalDate dateOfBirth;
+
+    public AppUser(String firstName, String lastName, String emailAdress, LocalDate dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAdress = emailAdress;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+
+}
