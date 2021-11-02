db257006b4f8c04b2da3d3719b6284d85ec71955
Zanghetsu <boythasx@gmail.com>
2021-11-02 09:57:06 +0100

    Refactored appUser- Entitiy + added id sequence

--- src/main/java/com/restaurantmanager/usermanager/appuser/AppUser.java
+++ src/main/java/com/restaurantmanager/usermanager/appuser/AppUser.java
@@ -3,13 +3,24 @@ package com.restaurantmanager.usermanager.appuser;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.time.LocalDate;
+
+
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Entity
 public class AppUser {
+
+
+    @Id
+    @SequenceGenerator(name = "app_user_sequence", sequenceName = "app_user_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "app_user_sequence")
     private int id;
+
     private String firstName;
     private String lastName;
     private String emailAdress;
